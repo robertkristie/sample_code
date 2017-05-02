6850dac726bda0be853997ffc91cee9de4a81bb7
Rob Kristie <rob.kristie@zoho.com>
2017-05-01 21:51:55 -0700

    Initial commit

---
+++ comment_email/comment_notification.php
@@ -0,0 +1,234 @@
+<?php
+/*
+Plugin Name: email comment notifications to editors
+Plugin URI: https://github.com/robertkristie
+Description: This is the custom email management tool
+Version: 1.0
+Author: Rob Kristie
+Author URI: https://github.com/robertkristie
+*/
+
+//Emails for when a comment is put in moderation
+function moderation($comment_id) {
+	global $wpdb;
+	//$mna_settings = mna_read_options();
+	//if( !$mna_settings[moderate_author] ) return true; // Exit if option disabled
+	$comment = get_comment($comment_id);
+	if( '0' != $comment->comment_approved ) return true; // Exit if comment is approved	
+	$post = get_post($comment->comment_post_ID); 
+	$authorid = $post->post_author;	
+	$authorname = get_the_author_meta( 'display_name', $authorid );   
+	$category = get_the_category( $post );
+	$siteurl = get_option('siteurl');
+	$catid = $category[0]->cat_ID;
+	$catname = $category[0]->cat_name;
+	$catvalue = '"' . $catid . '"';
+	$myrows = $wpdb->get_results( "SELECT * FROM wp_usermeta WHERE meta_key = 'user_cats' AND meta_value LIKE '%$catvalue%'" );
+	$editors = array();	
+	foreach ($myrows as $myrow) {
+		$user_id = $myrow->user_id;
+		$userlevel1 = 'wp_user_level';
+  		$single = true;
+  		$user_roler = get_user_meta( $user_id, $userlevel1, $single );  	
+  		if ($user_roler >6){
+  			$user_info = get_userdata($user_id);
+  			$editoremail = $user_info ->user_email;
+  			$editors[] = $editoremail; 
+  		}
+	}
+	$admins = get_users('role=administrator');
+	foreach ($admins as $admin) {
+		$user_id = $admin->ID;
+  		$key = 'admin_email';
+  		$single = true;
+  		$user_admin = get_user_meta( $user_id, $key, $single ); 
+  		if ($user_admin =='true') {
+  			$user_info = get_userdata($user_id);
+  			$editoremail = $user_info ->user_email;
+  			$editors[] = $editoremail;  		
+  		}		
+	}
+	$emails = implode(", ",$editors);
+	
+
+	$notify_message = sprintf( __('A new comment on the post #%1$s "%2$s" is waiting for your approval'), $post->ID, $post->post_title ) . "\r\n";
+	$notify_message .= sprintf( __('Post Author : %s'), $authorname ) . "\r\n";
+	$notify_message .= get_permalink($comment->comment_post_ID) . "\r\n\r\n";
+	$notify_message .= sprintf( __('Author : %1$s '), $comment->comment_author) . "\r\n";
+	$notify_message .= sprintf( __('E-mail : %s'), $comment->comment_author_email ) . "\r\n";
+	$notify_message .= sprintf( __('URL    : %s'), $comment->comment_author_url ) . "\r\n";
+	$notify_message .= __('Comment: ') . "\r\n" . $comment->comment_content . "\r\n\r\n";
+	$notify_message .= sprintf( __('Approve it: %s'),  "http://blogs.cisco.com/wp-admin/comment.php?action=mac&c=$comment_id" ) . "\r\n";
+	$notify_message .= sprintf( __('Delete it: %s'), "http://blogs.cisco.com/wp-admin/comment.php?action=cdc&c=$comment_id" ) . "\r\n";
+	$notify_message .= sprintf( __('Spam it: %s'), "http://blogs.cisco.com/wp-admin/comment.php?action=cdc&dt=spam&c=$comment_id" ) . "\r\n";
+	
+
+	$subject = 'A comment has been posted and needs moderation in the ' . $catname . ' blog';
+
+
+	$notify_message = apply_filters('comment_moderation_text', $notify_message, $comment_id);
+	$subject = apply_filters('comment_moderation_subject', $subject, $comment_id);
+
+	@wp_mail($emails, $subject, $notify_message);
+
+	return true;
+}
+
+add_action('comment_post', 'moderation');
+
+//emails for when a comment in moderation is approved
+
+
+function approved_alert($comment) {
+	global $wpdb;
+	$comment2 = get_comment($comment);
+	$comment_id = $comment2->comment_ID;
+	$commentid = $comment2->comment_post_ID;
+	$post = get_post($comment->comment_post_ID); 
+	$authorid = $post->post_author;	
+	$authorname = get_the_author_meta( 'display_name', $authorid );  
+    $category = get_the_category( $commentid  );
+    $catid = $category[0]->cat_ID;
+    $catname = $category[0]->cat_name;
+    $catvalue = '"' . $catid . '"';
+	$myrows = $wpdb->get_results( "SELECT * FROM wp_usermeta WHERE meta_key = 'user_cats' AND meta_value LIKE '%$catvalue%'" );
+	$editors = array();	
+	
+	foreach ($myrows as $myrow) {
+		$user_id = $myrow->user_id;
+		$userlevel1 = 'wp_user_level';
+  		$single = true;
+  		$user_roler = get_user_meta( $user_id, $userlevel1, $single );  	
+  		if ($user_roler >6){
+  			$user_info = get_userdata($user_id);
+  			$editoremail = $user_info ->user_email;
+  			$editors[] = $editoremail; 
+  		}
+	}
+
+	$admins = get_users('role=administrator');
+	foreach ($admins as $admin) {
+		$user_id = $admin->ID;
+  		$key = 'admin_email';
+  		$single = true;
+  		$user_admin = get_user_meta( $user_id, $key, $single ); 
+  		if ($user_admin =='true') {
+  			$user_info = get_userdata($user_id);
+  			$editoremail = $user_info ->user_email;
+  			$editors[] = $editoremail;  		
+  		}		
+	}
+	$emails = implode(", ",$editors);
+	$notify_message = sprintf( __('A comment on the post #%1$s "%2$s" has been approved'), $post->ID, $post->post_title ) . "\r\n";
+	$notify_message .= sprintf( __('Post Author : %s'), $authorname ) . "\r\n";
+	$notify_message .= get_permalink($comment2->comment_post_ID) . "\r\n\r\n";
+	$notify_message .= sprintf( __('Author : %1$s '), $comment2->comment_author) . "\r\n";
+	$notify_message .= sprintf( __('E-mail : %s'), $comment2->comment_author_email ) . "\r\n";
+	$notify_message .= sprintf( __('URL    : %s'), $comment2->comment_author_url ) . "\r\n";
+	$notify_message .= __('Comment: ') . "\r\n" . $comment2->comment_content . "\r\n\r\n";
+	$notify_message .= sprintf( __('Delete it: %s'), "http://blogs.cisco.com/wp-admin/comment.php?action=cdc&c=$comment_id" ) . "\r\n";
+	$notify_message .= sprintf( __('Spam it: %s'), "http://blogs.cisco.com/wp-admin/comment.php?action=cdc&dt=spam&c=$comment_id" ) . "\r\n";
+	$subject = 'A comment has been approved in the ' . $catname . ' blog';
+
+		@wp_mail($emails, $subject, $notify_message);
+    	
+}
+
+add_action('comment_unapproved_to_approved', 'approved_alert');
+
+//emails for when a comment is auto approved
+
+function comment_alert($comment) {
+	global $wpdb;
+	$comment2 = get_comment($comment);
+	$approved = ($comment2->comment_approved);
+	$commentid = $comment2->comment_post_ID;
+	$comment_id = $comment2->comment_ID;
+	$post = get_post($comment2->comment_post_ID); 
+	$authorid = $post->post_author;	
+	$authorname = get_the_author_meta( 'display_name', $authorid );  
+    $category = get_the_category( $commentid);
+    $catid = $category[0]->cat_ID;
+    $catname = $category[0]->cat_name;
+    $catvalue = '"' . $catid . '"';
+	$myrows = $wpdb->get_results( "SELECT * FROM wp_usermeta WHERE meta_key = 'user_cats' AND meta_value LIKE '%$catvalue%'" );
+	$editors = array();	
+	
+	if ($approved ==1){
+		foreach ($myrows as $myrow) {
+			$user_id = $myrow->user_id;
+			$userlevel1 = 'wp_user_level';
+  			$single = true;
+  			$user_roler = get_user_meta( $user_id, $userlevel1, $single );  	
+  			if ($user_roler >6){
+  				$user_info = get_userdata($user_id);
+  				$editoremail = $user_info ->user_email;
+  				$editors[] = $editoremail; 
+  			}
+		}
+
+		$admins = get_users('role=administrator');
+		foreach ($admins as $admin) {
+			$user_id = $admin->ID;
+  			$key = 'admin_email';
+  			$single = true;
+  			$user_admin = get_user_meta( $user_id, $key, $single ); 
+  			if ($user_admin =='true') {
+  				$user_info = get_userdata($user_id);
+  				$editoremail = $user_info ->user_email;
+  				$editors[] = $editoremail;  		
+  			}		
+		}
+		$emails = implode(", ",$editors);
+		$notify_message = sprintf( __('A comment on the post #%1$s "%2$s" has been auto approved'), $post->ID, $post->post_title ) . "\r\n";
+		$notify_message .= sprintf( __('Post Author : %s'), $authorname ) . "\r\n";
+		$notify_message .= get_permalink($comment2->comment_post_ID) . "\r\n\r\n";
+		$notify_message .= sprintf( __('Author : %1$s '), $comment2->comment_author) . "\r\n";
+		$notify_message .= sprintf( __('E-mail : %s'), $comment2->comment_author_email ) . "\r\n";
+		$notify_message .= sprintf( __('URL    : %s'), $comment2->comment_author_url ) . "\r\n";
+		$notify_message .= __('Comment: ') . "\r\n" . $comment2->comment_content . "\r\n\r\n";
+		$notify_message .= sprintf( __('Delete it: %s'), "http://blogs.cisco.com/wp-admin/comment.php?action=cdc&c=$comment_id" ) . "\r\n";
+		$notify_message .= sprintf( __('Spam it: %s'), "http://blogs.cisco.com/wp-admin/comment.php?action=cdc&dt=spam&c=$comment_id" ) . "\r\n";
+		$subject = 'A comment has been posted and auto approved in the ' . $catname . ' blog';
+
+		@wp_mail($emails, $subject, $notify_message);
+
+	}
+	
+	
+}
+
+add_action ('comment_post', 'comment_alert');
+
+
+//Adds custom page to select admins to receive all emails
+
+//add menu item
+add_action( 'admin_menu', 'email_menu' );
+
+function email_menu() {
+	add_users_page( 'Admins to recieve emails', 'Admins', 'manage_options', 'admins', 'email_options' );
+}
+function email_options(){
+	$admins = get_users('role=administrator');
+	echo '<p>Select the admins you wish to receive comment notification emails</p>'; ?>
+	<?php echo '<ul>';
+		foreach( $admins as $admin) {
+		echo '<li>';
+			$user_id = $admin->ID;
+  			$key = 'admin_email';
+  			$single = true;
+  			$user_admin = get_user_meta( $user_id, $key, $single ); 
+		?>
+		<input value="true" name="admin_email" type="checkbox"
+		<?php if ($user_admin =='true') { ?> checked="checked"<?php  } ?>
+	</input>
+	<?php echo $admin->first_name; echo " "; echo $admin->last_name;
+	echo '</li>';
+	} 
+	?>
+	</ul>
+	</table>
+<?php
+}
+?>
---
+++ scac/simple_category_access.php
@@ -0,0 +1,248 @@
+<?php
+/*
+Plugin Name: Simple Category Access Control
+Plugin URI: https://github.com/robertkristie
+Description: Author category access.
+Version: 1.0
+Author: Rob Kristie
+Author URI: https://github.com/robertkristie
+*/
+
+
+add_action( 'show_user_profile', 'show_extra_profile_fields' );
+add_action( 'edit_user_profile', 'show_extra_profile_fields');
+ 
+function show_extra_profile_fields( $user ) { 
+	if (current_user_can('administrator')){?>
+ 		<table class="form-table">
+			<tr>
+				<th><label for="user_cats">Categories</label></th>
+				<td>
+					<?php $user_cats = get_the_author_meta( 'user_cats', $user->ID ); ?>
+					<ul>
+						<?php 
+						$categories = get_categories('orderby=name');
+						foreach ($categories as $category){ ?>	
+							<li><input value="<?php echo $category->cat_ID; ?>" name="user_cats[]" <?php if (is_array($user_cats)) { if (in_array($category->cat_ID,$user_cats)) { ?>checked="checked"<?php } }?> type="checkbox" /> <?php echo $category->cat_name ?>
+							</li>
+						<?php } ?>
+					</ul>
+				</td>			
+			</tr>
+ 		</table>
+	<?php }
+	else { ?>
+		<div style = "display:none;visibility:hidden;">
+			<table class="form-table">
+				<tr>
+					<th><label for="user_cats">Categories</label></th>
+					<td>
+						<?php $user_cats = get_the_author_meta( 'user_cats', $user->ID ); ?>
+						<ul>
+							<?php 
+							$categories = get_categories('orderby=name');
+							foreach ($categories as $category){ ?>	
+							<li><input value="<?php echo $category->cat_ID; ?>" name="user_cats[]" <?php if (is_array($user_cats)) { if (in_array($category->cat_ID,$user_cats)) { ?>checked="checked"<?php } }?> type="checkbox" /> <?php echo $category->cat_name ?>
+							</li>
+							<?php } ?>
+						</ul>
+					</td>			
+				</tr>
+ 			</table>
+ 		</div>
+	<?php
+	}
+}
+ 
+add_action( 'personal_options_update', 'save_extra_profile_fields' );
+add_action( 'edit_user_profile_update', 'save_extra_profile_fields' );
+ 
+function save_extra_profile_fields( $user_id ) {
+	if ( !current_user_can( 'edit_user', $user_id ) ) {
+		return false;
+	}
+	update_usermeta( $user_id, 'user_cats', $_POST['user_cats'] );
+}
+ 
+
+/*
+ * Show authors only their posts
+ */
+
+function posts_for_current_author($query) {
+	global $user_level;
+
+	if($query->is_admin && $user_level < 5) {
+		global $user_ID;
+		$query->set('author',  $user_ID);
+		unset($user_ID);
+	}
+	unset($user_level);
+
+	return $query;
+}
+add_filter('pre_get_posts', 'posts_for_current_author');
+
+
+/*
+ * Show only the Comments MADE BY the current logged user
+ * and the Comments MADE TO his/hers posts or to categories current user manages
+ * 
+ */
+function comment_list_by_user($clauses) {
+
+	global $user_ID; 
+	get_currentuserinfo();
+	if (current_user_can('author')||current_user_can('contributor')) {
+		
+			global $user_ID, $wpdb;
+			$clauses['join'] = ", wp_posts";
+			$clauses['where'] .= " AND wp_posts.post_author = ".$user_ID." AND wp_comments.comment_post_ID = wp_posts.ID";		
+		return $clauses;
+	}
+	else {
+		
+			global $wpdb;
+			$clauses['join'] = ", wp_posts";
+			$clauses['where'] .= " AND wp_comments.comment_post_ID = wp_posts.ID";
+			return $clauses;
+		
+		
+	}
+
+};
+
+if (is_admin()) {
+add_filter('comments_clauses', 'comment_list_by_user');
+}
+
+
+/*
+ * Hide all categories with CSS, show allowed categories
+ */
+function hide_the_categories(){
+
+	if( is_admin() ) {
+    	global $pagenow;
+     	if( 'edit.php' == $pagenow || 'post-new.php' == $pagenow || 'post.php' == $pagenow) {
+    		if( current_user_can('author') || current_user_can('editor') || current_user_can('contributor') ) {
+    			?>
+    			<style type="text/css">
+        			ul#categorychecklist li, ul#categorychecklist-pop li{
+            		display:none;
+        			}
+    			</style>
+				<?php
+			}
+			global $wp_roles;
+    		$current_user = wp_get_current_user();
+    		//$roles = $current_user->roles;
+    		//$role = array_shift($roles);
+    		$user_id =$current_user->ID;
+    		$key = "user_cats";
+    		$single = "false";
+    		$usercats = get_user_meta($user_id, $key, $single);
+    		if (!empty($usercats)) {
+    			foreach ($usercats as $usercat) {
+    			echo '<style type="text/css">';
+    			echo 'ul#categorychecklist li#category-' . $usercat . '{';
+    			echo 'display:block;';
+    			echo '}';
+    			echo '</style>';
+			}	 
+		}
+       
+    }
+}
+
+
+	
+}
+
+add_action( "admin_head", "hide_the_categories" );
+
+
+//add menu item
+add_action( 'admin_menu', 'roles_menu' );
+
+function roles_menu() {
+	add_users_page( 'Roles By Category', 'Roles', 'manage_options', 'roles', 'roles_options' );
+}
+
+function roles_options() {
+	if ( !current_user_can( 'manage_options' ) )  {
+		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+	}
+	echo '<div>';
+		$args=array(
+  			'orderby' => 'name',
+  			'order' => 'ASC'
+  		);
+		$categories=get_categories($args);
+  		foreach($categories as $category) {
+  			echo '<h3>'; 
+  			echo $category->name;
+  			echo '</h3>';
+  			$cat_id = $category->cat_ID;
+  			$catvalue = '"' . $cat_id . '"';
+  			//echo $catvalue;
+  			global $wpdb;
+  			$myrows = $wpdb->get_results( "SELECT * FROM wp_usermeta WHERE meta_key = 'user_cats' AND meta_value LIKE '%$catvalue%'" );
+  			$authors = array();
+			$editors = array();	
+			//echo $cat_id;  			
+  			foreach ($myrows as $myrow) {
+				$user_id = $myrow->user_id;
+				$lastname = 'last_name';
+				$firstname = 'first_name';
+				$userlevel1 = 'wp_user_level';
+  				$single = true;
+  				$user_first = get_user_meta( $user_id, $firstname, $single );
+  				$user_last = get_user_meta( $user_id, $lastname, $single );
+  				$user_roler = get_user_meta( $user_id, $userlevel1, $single );
+  				$username = $user_first . " " . $user_last;
+  				//echo $username;
+  				if ($user_roler >6){
+  					$editors[] = $username; 
+  				}
+  				else {
+  					$authors[] = $username;
+  				}
+			}
+			echo '<b>Editors</b>';
+			echo '<br />';
+			echo implode(", ",$editors);
+			echo '<br />';
+			echo '<b>Authors</b>';
+			echo '<br />';
+			echo implode(", ",$authors);
+			echo '<hr>';
+  		}
+	echo '</div>';
+}
+add_action( 'admin_head-post.php', 'disable_and_check_cat' );
+add_action( 'admin_head-post-new.php', 'disable_and_check_cat' );
+
+function disable_and_check_cat() {
+	if( current_user_can('author') || current_user_can('editor') || current_user_can('contributor') ) {
+		global $wp_roles;
+    	$current_user = wp_get_current_user();
+    	$user_id =$current_user->ID;
+    	$key = "user_cats";
+    	$single = "false";
+    	$usercats = get_user_meta($user_id, $key, $single);
+    	foreach ($usercats as $usercat) {
+		?>
+			<script type="text/javascript">
+				var cat = <?php echo $usercat;?>;
+				jQuery(document).ready(function($){
+					var required_cat = $('input#in-category-' + cat + '');
+					if( !required_cat.attr('checked')  )
+						required_cat.attr('checked','checked');
+				});
+			</script>
+		<?php
+		}
+	}
+}
+?>
